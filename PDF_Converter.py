f91c98599633672f6c97c4ab6a6ff1d924fbf5f3
smcgovern2 <smcgovern2@my.wctc.edu>
2023-10-01 09:45:39 -0500

    cleanup

--- PDF_Converter.py
+++ PDF_Converter.py
@@ -1,7 +1,7 @@
 
 # Import Module 
 import tabula
-import pandas as pd
+import pandas as pd #TODO enforce version number for deprecation
 import datetime as dt
 
 #Configs
@@ -10,19 +10,80 @@ date_max = dt.date.today()
 date_min = dt.date(2022,1,1)
 
 #Functions
-
+#Also converts values to string
 def clean_trailing_zeroes(df, i, columns):
-    for col in columns:
+     for col in columns:
         try:
             df.at[i,col] = str(df.loc[i,col]).replace(".0","")
         except(ValueError):
             print('non numeric ' + col + ' at index ' + i )
             
-#TODO: Validation to functions to reuse on master record
+def validate_currency_range(val, max, min):
+    
+    if val.startswith("$"):
+        val = val.replace("$","").strip()
+    if not ((val.isnumeric() or val.replace(".", "").isnumeric()) and min <= float(val) <= max):
+        return True
+    else:
+        return False
+        
+def validate_dataframe (df):
+    #Removes duplicate shipment IDs 
+    try: df = df.drop_duplicates(subset=['Shipment ID'])
+                                                    
+    except:
+        print('Duplicate test failure')
 
+    df.reset_index(drop=True,inplace=True)
+    
+    drop_indexes = []
+    
+    for i in df.index:             
+        
+        #Standardizes values that are sometimes extracted as numpy.float, sets as str
+        clean_trailing_zeroes(df_processed, i, ['Zip','Order Number', 'PO Number', 'Shipment ID'])     
+            
+            
+        #Allows current row until flipped      
+        failed = False
+        
+        #Date-range Check
+        try:
+            date_df = dt.datetime.strptime((df_processed.loc[i,'Shipped Date']),'%m/%d/%Y').date()
+            if not (date_min <= date_df <= date_max):
+                failed = True
+                print("Invalid value in shipped date at index " + str(i))
+        except ValueError:
+            failed = True
+            print("Invalid value in shipped date at index " + str(i))
+            
+            
+        #Zip Check, verifies numeric and length     
+        if not (df_processed.loc[i,'Zip'].isnumeric() and 5 <= len(df_processed.loc[i,'Zip']) <= 9):
+            failed = True
+            print("Invalid value in ZIP at index " + str(i))
+            
+        #Order Value Check
+        order_value = df_processed.loc[i,'Order Value']
+        if not failed:
+            failed = validate_currency_range(order_value,10000,0)
+        
+        #Shipping cost check
+        shipping_cost = df_processed.loc[i,'Shipping Cost']
+        if not failed:
+            failed = validate_currency_range(shipping_cost,1000,0)
+            
+        if failed == True:
+            drop_indexes.append(i)
+            
+    print(drop_indexes)
 
-#Alter dataframe for validation testing
-def poison_data(df):
+    df.drop(index=drop_indexes,inplace=True, axis=0)        
+    
+    df = df.reset_index(drop=True)
+
+#Alter dataframe for validation testing        
+def poison_data(df):    
     df.at[10,'Shipped Date'] = '01/01/2000'
     df.at[20,'Shipped Date'] = '01/01/2030'
     df.at[55, 'Shipped Date'] = 'AAA'
@@ -44,9 +105,6 @@ def poison_data(df):
     return df
 
     
-
-
-
 # input_path = input("Input path:/n")
 # output_path = input("output path:/n")
 
@@ -61,15 +119,17 @@ df = tabula.read_pdf(input_path, pages='all', stream=True)
 
 df_processed = pd.DataFrame ()
 
+
+
 for page in df: 
     
-    #Spilts Customer ID and ZIP columns when tabula recognizes as single column due to low whitespace, split on final whitespace char, whitespace in ZIP will break this
+    #Spilts Customer ID and ZIP columns when tabula recognizes as single column due to low whitespace, split on final whitespace char
     try:
         page[['Customer ID', 'Zip']] = page["Customer ID Zip"].apply(lambda x: pd.Series(str(x).rsplit(" ", 1)))
         page = page.drop('Customer ID Zip', axis=1)
     except:
         print("CIDZIP Pass")
-    #Extractor returns empty column from some input files
+    #Adresses extractor returning empty column from some input files
     try:
         page = page.drop('Unnamed: 0', axis=1)
     except:
@@ -91,7 +151,6 @@ for page in df:
     
     df_processed = pd.concat([df_processed, page], axis=0)
     
-    
 df_processed.reset_index(drop=True,inplace=True)   
 
 df_processed = poison_data(df_processed)
@@ -106,58 +165,9 @@ except:
 df_processed.reset_index(drop=True,inplace=True)
 
  #Validation 
+validate_dataframe(df_processed)
 
-drop_indexes = []
-    
-for i in df_processed.index:             
-    
-    #Standardizes values that are sometimes extracted as numpy.float, sets as str
-    clean_trailing_zeroes(df_processed, i, ['Zip','Order Number', 'PO Number', 'Shipment ID'])     
-          
-          
-    #Allows current row until flipped      
-    failed = False
-    
-    #Date-range Check
-    try:
-        date_df = dt.datetime.strptime((df_processed.loc[i,'Shipped Date']),'%m/%d/%Y').date()
-        if not (date_min <= date_df <= date_max):
-            failed = True
-            print("Invalid value in shipped date at index " + str(i))
-    except ValueError:
-        failed = True
-        print("Invalid value in shipped date at index " + str(i))
-        
-        
-    #Zip Check, verifies numeric and length     
-    if not (df_processed.loc[i,'Zip'].isnumeric() and 5 <= len(df_processed.loc[i,'Zip']) <= 9):
-        failed = True
-        print("Invalid value in ZIP at index " + str(i))
-        
-    #Order Value Check
-    order_value = df_processed.loc[i,'Order Value']
-    if order_value.startswith("$"):
-        order_value = order_value.replace("$","").strip()
-    if not ((order_value.isnumeric() or order_value.replace(".", "").isnumeric()) and 0 <= float(order_value) <=10000):
-        failed = True
-        print("Invalid value in order value at index " + str(i))
-        
-    #Shipping cost check
-    shipping_cost = df_processed.loc[i,'Shipping Cost']
-    if shipping_cost.startswith("$"):
-        shipping_cost = shipping_cost.replace("$","").strip()
-    if not ((shipping_cost.isnumeric() or shipping_cost.replace(".", "").isnumeric()) and 0 <= float(shipping_cost) <=1000):
-        failed = True
-        print("Invalid value in shipping cost at index " + str(i))
-    
-    if failed == True:
-        drop_indexes.append(i)
-        
-print(drop_indexes)
 
-df_processed.drop(index=drop_indexes,inplace=True, axis=0)        
-
-df_processed = df_processed.reset_index(drop=True)
 
 df_processed.to_csv(output_path, mode='w')
 
