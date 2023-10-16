d90dd55a8fd8b86001794bd990b163dd07de2742
smcgovern2 <smcgovern2@my.wctc.edu>
2023-10-16 14:41:01 -0500

    Finished grouping and averaging logic

--- PDF_Converter.py
+++ PDF_Converter.py
@@ -4,6 +4,7 @@ import tabula
 import pandas as pd #TODO enforce version number for deprecation
 import datetime as dt
 import warnings
+import numpy as np
 #Configs
 
 warnings.filterwarnings('ignore', category=FutureWarning)
@@ -11,11 +12,188 @@ warnings.filterwarnings('ignore', category=FutureWarning)
 date_max = dt.date.today()
 date_min = dt.date(2022,1,1)
 
+
+
 df_columns=['Shipped Date','Customer ID','Zip','Shipment ID','Order Number','PO Number', 'Order Value', 'Shipping Cost']
 
 poison = False
 
 skip_prompt = True
+
+rmins = [
+    0.00,
+    12.00,
+    15.00,
+    25.00,
+    35.00,
+    45.00,
+    55.00,
+    75.00,
+    100.00,
+    125.00,
+    150.00,
+    175.00,
+    200.00,
+    225.00,
+    250.00,
+    275.00,
+    300.00,
+    325.00,
+    350.00,
+    375.00,
+    400.00,
+    425.00,
+    450.00,
+    475.00,
+    500.00,
+    525.00,
+    550.00,
+    575.00,
+    600.00,
+    625.00,
+    650.00,
+    675.00,
+    700.00,
+    725.00,
+    750.00,
+    775.00,
+    800.00,
+    825.00,
+    850.00,
+    875.00,
+    900.00,
+    925.00,
+    950.00,
+    975.00,
+    1000.00,
+    1025.00,
+    1050.00,
+    1075.00,
+    1100.00,
+    1125.00,
+    1150.00,
+    1175.00,
+    1200.00,
+    1225.00,
+    1250.00,
+    1275.00,
+    1300.00,
+    1350.00,
+    1400.00,
+    1450.00,
+    1500.00,
+    1550.00,
+    1600.00,
+    1650.00,
+    1700.00,
+    1750.00,
+    1800.00,
+    1850.00,
+    1900.00,
+    2000.00,
+    2250.00,
+    2500.00,
+    2750.00,
+    3000.00,
+    3500.00,
+    4000.00,
+    4500.00,
+    5000.00,
+    5500.00,
+    6000.00,
+    6500.00,
+    7000.00,
+    8000.00,
+    np.inf
+]
+
+rlabels = [
+    "$0.00 - $11.99",
+    "$12.00 - $14.99",
+    "$15.00 - $24.99",
+    "$25.00 - $34.99",
+    "$35.00 - $44.99",
+    "$45.00 - $54.99",
+    "$55.00 - $74.99",
+    "$75.00 - $99.99",
+    "$100.00 - $124.99",
+    "$125.00 - $149.99",
+    "$150.00 - $174.99",
+    "$175.00 - $199.99",
+    "$200.00 - $224.99",
+    "$225.00 - $249.99",
+    "$250.00 - $274.99",
+    "$275.00 - $299.99",
+    "$300.00 - $324.99",
+    "$325.00 - $349.99",
+    "$350.00 - $374.99",
+    "$375.00 - $399.99",
+    "$400.00 - $424.99",
+    "$425.00 - $449.99",
+    "$450.00 - $474.99",
+    "$475.00 - $499.99",
+    "$500.00 - $524.99",
+    "$525.00 - $549.99",
+    "$550.00 - $574.99",
+    "$575.00 - $599.99",
+    "$600.00 - $624.99",
+    "$625.00 - $649.99",
+    "$650.00 - $674.99",
+    "$675.00 - $699.99",
+    "$700.00 - $724.99",
+    "$725.00 - $749.99",
+    "$750.00 - $774.99",
+    "$775.00 - $799.99",
+    "$800.00 - $824.99",
+    "$825.00 - $849.99",
+    "$850.00 - $874.99",
+    "$875.00 - $899.99",
+    "$900.00 - $924.99",
+    "$925.00 - $949.99",
+    "$950.00 - $974.99",
+    "$975.00 - $999.99",
+    "$1,000.00 - $1,024.99",
+    "$1,025.00 - $1,024.99",
+    "$1,050.00 - $1,074.99",
+    "$1,075.00 - $1,099.99",
+    "$1,100.00 - $1,124.99",
+    "$1,125.00 - $1,149.99",
+    "$1,150.00 - $1,174.99",
+    "$1,175.00 - $1,199.99",
+    "$1,200.00 - $1,224.99",
+    "$1,225.00 - $1,249.99",
+    "$1,250.00 - $1,274.99",
+    "$1,275.00 - $1,299.99",
+    "$1,300.00 - $1,349.99",
+    "$1,350.00 - $1,399.99",
+    "$1,400.00 - $1,449.99",
+    "$1,450.00 - $1,499.99",
+    "$1,500.00 - $1,549.99",
+    "$1,550.00 - $1,599.99",
+    "$1,600.00 - $1,649.99",
+    "$1,650.00 - $1,699.99",
+    "$1,700.00 - $1,749.99",
+    "$1,750.00 - $1,799.99",
+    "$1,800.00 - $1,849.99",
+    "$1,850.00 - $1,899.99",
+    "$1,900.00 - $1,999.99",
+    "$2,000.00 - $2,249.99",
+    "$2,250.00 - $2,499.99",
+    "$2,500.00 - $2,749.99",
+    "$2,750.00 - $2,999.99",
+    "$3,000.00 - $3,499.99",
+    "$3,500.00 - $3,999.99",
+    "$4,000.00 - $4,499.99",
+    "$4,500.00 - $4,999.99",
+    "$5,000.00 - $5,499.99",
+    "$5,500.00 - $5,999.99",
+    "$6,000.00 - $6,499.99",
+    "$6,500.00 - $6,999.99",
+    "$7,000.00 - $7,999.99",
+    "$8,000.00 and up"
+]
+
+
 #Functions
 #Also converts values to string
 def clean_trailing_zeroes(df, i, columns):
@@ -94,6 +272,45 @@ def validate_dataframe (df):
     
     return df
 
+def group_by_order_value(df):
+    df_local = df.copy()
+    for col in df_local.columns:
+        if df_local[col].dtype == 'object' and '$' in df_local[col].iloc[0]:
+            df_local[col] = df_local[col].str.replace('$', '')
+            
+    df_local['Order Value'] = pd.to_numeric(df_local['Order Value'])
+    df_local['Shipping Cost'] = pd.to_numeric(df_local['Shipping Cost'])
+    
+    grouped_df = pd.DataFrame(columns=['Range', 'Average Shipping Cost'])
+    grouped_df['Range'] = pd.Series(rlabels)
+    
+    i = 1
+    avgs=[]
+    while i <= len(rmins) - 1:
+        avg = 0
+        inrange = pd.DataFrame()
+        
+        try:
+            if i < len(rmins):
+                inrange = df_local[df_local['Order Value'].between(rmins[i-1],rmins[i]-0.01)]
+                avg = inrange['Shipping Cost'].mean()
+            if  i == len(rmins):
+                inrange = df_local[df_local['Order Value'].ge(rmins[i-1])]
+                avg = inrange['Shipping Cost'].mean()
+        except KeyError:
+            avg = 0
+            
+        if(pd.isna(avg)):
+            avg = 0
+        avgs.append(avg)
+        i += 1
+    
+    grouped_df['Average Shipping Cost'] = avgs
+        
+    print(grouped_df.to_string())
+        
+    
+
 #Alter dataframe for validation testing        
 def poison_data(df):    
     df.at[10,'Shipped Date'] = '01/01/2000'
@@ -121,8 +338,8 @@ if not skip_prompt:
     output_path = input("output path:\n")
 
 if skip_prompt:
-    input_path = "reports/test data.pdf"
-    output_path = "results/test data.csv"
+    input_path = "reports/Test Data.pdf"
+    output_path = "results/Test Data.csv"
 
 mr_path = "results/master_record.csv"
 
@@ -191,11 +408,15 @@ if poison:
 
 df_processed = validate_dataframe(df_processed)
 
+group_by_order_value(df_processed)
+
 df_processed.to_csv(output_path, mode='w')
 
 df_master = pd.concat([df_master,df_processed], axis=0)
 
 df_master = validate_dataframe(df_master)
+
+group_by_order_value (df_master)
 
 df_processed.to_csv(mr_path, mode='w')
     
